6334b48d88cd8bb2e2c7035b514097d157c21644
Slo <sloversion2@gmail.com>
2020-10-04 00:53:45 +0700

    Reconstruct project and make game interactive

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,12 +3,19 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import model.Game;
+import view.View;
 
 public class Controller {
     @FXML
     private Canvas canvas;
+
+    private Game game = new Game();
+    private View view;
+    private double blockWidth;
+    private double blockHeight;
 
     @FXML
     public void initialize() {
@@ -17,16 +24,26 @@ public class Controller {
         gc.setLineWidth(3);
         double width = canvas.getWidth();
         double height = canvas.getHeight();
-        double blockWidth = width / 3;
-        double blockHeight = height / 3;
+        blockWidth = width / 3;
+        blockHeight = height / 3;
         // Draw board
         gc.strokeLine(blockWidth, 0, blockWidth, height);
         gc.strokeLine(blockWidth * 2, 0, blockWidth * 2, height);
         gc.strokeLine(0, blockHeight, width, blockHeight);
         gc.strokeLine(0, blockHeight * 2, width, blockHeight * 2);
         //Start the game
-        Game game = new Game();
-        game.start(gc,width,height,blockWidth,blockHeight);
+        view = new View(gc, blockWidth, blockHeight);
+        game.start(view);
     }
+
+    @FXML
+    public void clicked(MouseEvent event) {
+        double x = event.getX();
+        double y = event.getY();
+        int j = (int) (x/blockWidth);
+        int i = (int) (y/blockHeight);
+        game.nextTurn(i,j);
+    }
+
 
 }
--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,30 +1,24 @@
 package model;
 
 import javafx.scene.canvas.GraphicsContext;
+import view.View;
 
 public class Game {
     public final static int X = 1;
     public final static int O = 2;
-    public int[][] board = {{X, O, X}, {O, X, O}, {X, O, X}};
-    public void start(GraphicsContext gc, double width, double height, double blockWidth, double blockHeight) {
-        repaint(gc, width, height, blockWidth, blockHeight);
+    public int[][] board = new int[3][3];
+    public int turn=X;
+    public View view ;
+    public void start(View view) {
+        this.view = view;
     }
-//Paint X and O
-    public void repaint(GraphicsContext gc, double width, double height, double blockWidth, double blockHeight) {
-        for (int i = 0; i < board.length; i++) {
-            double yTopLeft = i * blockHeight + (blockHeight * 1 / 6);
-            double yBottomRight = i * blockHeight + (blockHeight * 5 / 6);
-            for (int j = 0; j < board[i].length; j++) {
-                double xTopLeft = j * blockWidth + (blockWidth * 1 / 6);
-                double xBottomRight = j * blockWidth + (blockWidth * 5 / 6);
-                switch (board[i][j]) {
-                    case O -> gc.strokeOval(xTopLeft, yTopLeft, blockWidth * 2 / 3, blockHeight * 2 / 3);
-                    case X -> {
-                        gc.strokeLine(xTopLeft, yTopLeft, xBottomRight, yBottomRight);
-                        gc.strokeLine(xTopLeft, yBottomRight, xBottomRight, yTopLeft);
-                    }
-                }
-            }
+
+    public void nextTurn(int i, int j) {
+        board[i][j] = turn;
+        switch (turn){
+            case X -> turn=O;
+            case O -> turn=X;
         }
+        view.repaint(board);
     }
 }
